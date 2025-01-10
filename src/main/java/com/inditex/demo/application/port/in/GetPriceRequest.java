2a3710334ae625f9973d2c62e89cf110563b3f60
Martín <martin1996zgz@gmail.com>
2025-01-10 17:14:17 +0100

    Implement getPrice logic inputs

---
+++ src/main/java/com/inditex/demo/application/port/in/GetPriceRequest.java
@@ -0,0 +1,23 @@
+package com.inditex.demo.application.port.in;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import static com.inditex.demo.common.validation.Validation.validate;
+
+public record GetPriceRequest(
+        @NotNull @NotBlank String startDate,
+        @Min(1) int productId,
+        @Min(1) int brandId
+) {
+    public GetPriceRequest(
+            String startDate,
+            int productId,
+            int brandId) {
+        this.startDate = startDate;
+        this.productId = productId;
+        this.brandId = brandId;
+        validate(this);
+    }
+}
---
+++ src/main/java/com/inditex/demo/application/port/in/GetPriceUseCase.java
@@ -0,0 +1,6 @@
+package com.inditex.demo.application.port.in;
+
+public interface GetPriceUseCase {
+
+    GetPriceResponse getPrice(GetPriceRequest request);
+}
