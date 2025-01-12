56ae4d70833b90853f55dbf694f380e163fe515b
Martín <martin1996zgz@gmail.com>
2025-01-12 15:17:40 +0100

    Fix brand mapping in PriceMapper

--- src/main/java/com/inditex/demo/adapter/out/persistance/PriceMapper.java
+++ src/main/java/com/inditex/demo/adapter/out/persistance/PriceMapper.java
@@ -1,8 +1,10 @@
 package com.inditex.demo.adapter.out.persistance;
 
+import com.inditex.demo.application.domain.model.Brand;
 import com.inditex.demo.application.domain.model.Price;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -16,6 +18,11 @@ public interface PriceMapper {
 
     @Mapping(source = "id.productId", target = "productId")
     @Mapping(source = "id.priceList", target = "priceList")
+    @Mapping(source = "id.brandId", target = "brandId", qualifiedByName = "mapFromBrandIdToBrand")
     Price mapToDomainEntity(PriceJPAEntity priceJPAEntity);
 
+    @Named("mapFromBrandIdToBrand")
+    public static Brand mapFromBrandIdToBrand(int brandId) {
+        return Brand.fromBrandId(brandId);
+    }
 }
